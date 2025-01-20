1220b67f42f15b96a27df8b60999836be00feead
omaroskars <omar@konstant.is>
2025-01-20 06:01:35 +0100

    cleanup

--- src/utils/generateUrl.ts
+++ src/utils/generateUrl.ts
@@ -1,3 +1,5 @@
+import type { PayloadRequest } from 'payload'
+
 import type { PluginContext } from './createPluginContext.js'
 
 export const generateUrl = (context: PluginContext, data: Partial<any> | undefined) => {
@@ -14,4 +16,28 @@ export const generateUrl = (context: PluginContext, data: Partial<any> | undefin
   }
 
   return generatedUrl
+}
+
+export const generateLocalizedUrl = ({
+  context,
+  data,
+  req,
+}: {
+  context: PluginContext
+  data: Partial<any> | undefined
+  req: PayloadRequest
+}): string => {
+  const { locale, payload } = req
+  const { defaultLocale = context.fallbackLocale } = payload.config.localization || {}
+
+  const url = generateUrl(context, data)
+
+  switch (context.appendLocaleToUrl) {
+    case 'all':
+      return `/${locale}${url}`
+    case 'exclude-default':
+      return locale === defaultLocale ? url : `/${locale}${url}`
+    default:
+      return url
+  }
 }
